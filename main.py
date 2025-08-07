79e485670c9bdc07f958a64492c88148eb6ef9a9
Cillian Myles <myles.cillian@gmail.com>
2025-08-07 19:53:22 +0100

    feat: Read book contents based on file path

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+def get_book_text(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return "Book not found. Please check the file path."
+
+main()
