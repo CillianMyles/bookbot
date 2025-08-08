d38ed5e2b74baef5af7f9bb0ce127d47acde1743
Cillian Myles <myles.cillian@gmail.com>
2025-08-08 19:36:00 +0100

    feat: Print the numbder of words in the text/document

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    words = count_number_of_words_in(text)
+    print(f"{words} words found in the document")
 
 def get_book_text(file_path):
     try:
@@ -7,5 +9,8 @@ def get_book_text(file_path):
             return file.read()
     except FileNotFoundError:
         return "Book not found. Please check the file path."
+    
+def count_number_of_words_in(text):
+    return len(text.split())
 
 main()
