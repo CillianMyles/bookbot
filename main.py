07c97355d3a7429b25e86524c38428645d1990fb
Cillian Myles <myles.cillian@gmail.com>
2025-08-08 19:43:38 +0100

    chore: Move function to count number of words in text to stats file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from stats import count_number_of_words_in_text
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    words = count_number_of_words_in(text)
+    words = count_number_of_words_in_text(text)
     print(f"{words} words found in the document")
 
 def get_book_text(file_path):
@@ -9,8 +11,5 @@ def get_book_text(file_path):
             return file.read()
     except FileNotFoundError:
         return "Book not found. Please check the file path."
-    
-def count_number_of_words_in(text):
-    return len(text.split())
 
 main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def count_number_of_words_in_text(text):
+    return len(text.split())
