be18aad3ad2cae977129963c5b5afbfe003fa898
Cillian Myles <myles.cillian@gmail.com>
2025-08-12 19:49:07 +0100

    feat: Produce book report (WIP)

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from stats import count_characters_in_text
+from stats import count_characters_in_text, count_number_of_words_in_text
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    counts = count_characters_in_text(text)
-    print(counts)
+    print("============ BOOKBOT ============")
+    book = "books/frankenstein.txt"
+    print(f"Analyzing book found at {book}")
+    text = get_book_text(book)
+    print("----------- Word Count ----------")
+    word_count = count_number_of_words_in_text(text)
+    print(f"Found {word_count} total words")
+    character_count = count_characters_in_text(text)
+    print("--------- Character Count -------")
+    print(character_count)
 
 def get_book_text(file_path):
     try:
--- stats.py
+++ stats.py
@@ -12,3 +12,9 @@ def count_characters_in_text(text):
             counts[char] = 0
         counts[char] += 1
     return counts
+
+def sort_character_counts(counts):
+    return counts.sort(reverse=True, key=_sort_on)
+
+def _sort_on(items):
+    return items["num"]
